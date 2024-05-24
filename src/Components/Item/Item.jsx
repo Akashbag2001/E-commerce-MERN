b546966094c611375249bf631cbaf6c6276f735a
AkashBag2024 <akash.bag@iemlabs.com>
2024-05-24 13:00:30 +0530

    Changes

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -7,8 +7,8 @@ function Item({ image, name, new_price, old_price }) {
       <img src={image} alt="Item_img" />
       <p>{name}</p>
       <div className="item_prices">
-        <div className="item_price_new">{new_price}</div>
-        <div className="item_price_old">{old_price}</div>
+        <div className="item_price_new">${new_price}</div>
+        <div className="item_price_old">${old_price}</div>
       </div>
     </div>
   );
